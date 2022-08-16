5b92fc2eca003947c30ec42c520429e9129ebe7f
Ujjwal Kumar Bhowmick <64903128+ujju2020@users.noreply.github.com>
2022-08-16 18:01:07 +0530

    done modification

--- browser.py
+++ browser.py
@@ -14,11 +14,11 @@ driver.find_element(By.ID,"userName").send_keys("subhrajyoti.pramanik@siti.essel
 driver.find_element(By.ID,"password").send_keys("BumbleB33*2021")
 driver.find_element(By.ID,"loginButton").click()
 time.sleep(2)
-driver.forward("https://d3p9d8t4dp35oz.cloudfront.net/#/subscription/client_users/users")
-driver.find_element(By.ID,"RMNID").send_keys("9830283204")
-driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/main/div/div/div[1]/div/div[4]/div[1]/button/span[1]").click()
+#driver.forward("https://d3p9d8t4dp35oz.cloudfront.net/#/subscription/client_users/users")
+#driver.find_element(By.ID,"RMNID").send_keys("9830283204")
+#driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/main/div/div/div[1]/div/div[4]/div[1]/button/span[1]").click()
 
-time.sleep(5)
+time.sleep(3)
 driver.close()
 #driver.quit()
 
