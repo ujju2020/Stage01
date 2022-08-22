7db541672b5c4e885c53b1e5ed770ab1fe9051af
Ujjwal Kumar Bhowmick <64903128+ujju2020@users.noreply.github.com>
2022-08-23 00:30:19 +0530

    testcommit02

--- browser.py
+++ browser.py
@@ -1,6 +1,5 @@
 import time
 from selenium import webdriver
-#import self
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
@@ -14,12 +13,12 @@ driver.find_element(By.ID,"userName").send_keys("subhrajyoti.pramanik@siti.essel
 driver.find_element(By.ID,"password").send_keys("BumbleB33*2021")
 driver.find_element(By.ID,"loginButton").click()
 time.sleep(2)
-driver.switch_to("https://d3p9d8t4dp35oz.cloudfront.net/#/subscription/client_users/users")
+#driver.switch_to("https://d3p9d8t4dp35oz.cloudfront.net/#/subscription/client_users/users")
 #driver.find_element(By.ID,"RMNID").send_keys("9830283204")
 #driver.find_element(By.XPATH,"//*[@id='root']/div/div[2]/main/div/div/div[1]/div/div[4]/div[1]/button/span[1]").click()
 
 time.sleep(3)
-driver.close()
-#driver.quit()
+#driver.close()
+driver.quit()
 
 
